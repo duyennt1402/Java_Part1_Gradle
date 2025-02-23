5baaee20d1c37e003ad71cbe9a087db8f97a148d
duyennt1402 <72242902+duyennt1402@users.noreply.github.com>
2025-02-23 12:20:10 +0700

    update Main and Cityzen class

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        Scanner sc1 = new Scanner(System.in);
-        System.out.println("input name of Cityzen");
-        String name = sc1.nextLine();
-        Scanner sc2 = new Scanner(System.in);
-        System.out.println("input age:");
-        int age = sc2.nextInt();
-        Scanner sc3 = new Scanner(System.in);
-        System.out.println("input gender:");
-        String gender = sc3.nextLine();
-        Cityzen citizen1 = new Cityzen(name, age, gender);
+        // input data from keyboard
+//        Scanner sc1 = new Scanner(System.in);
+//        System.out.println("input name of Cityzen");
+//        String name = sc1.nextLine();
+//        Scanner sc2 = new Scanner(System.in);
+//        System.out.println("input age:");
+//        int age = sc2.nextInt();
+//        Scanner sc3 = new Scanner(System.in);
+//        System.out.println("input gender:");
+//        String gender = sc3.nextLine();
+//        Cityzen citizen1 = new Cityzen(name, age, gender);
+        // data is hardcode
+        Cityzen citizen1 = new Cityzen("Duyen", 38, "Nu");
         citizen1.showInfo();
 
     }
